5b52e4e8e5f7a2fa99550c444d57feb8d8df4822
StefanKunde <stefan-kunde@gmx.de>
2014-05-05 02:34:26 +0200

    Falsche Usereingaben werden nun behandelt. Die Ausgabe des Menüs wurde an die neue Struktur angepasst.

--- Quizilla/src/Menu.java
+++ Quizilla/src/Menu.java
@@ -3,38 +3,40 @@ import java.util.Scanner;
 
 public class Menu 
 {
-	final String PLAY_GAME_TEXT = "Spiel starten";
-	final String HIGH_SCORE_TEXT = "Highscore";
-	final String MANUAL_TEXT = "Anleitung";
-	final String EXIT_GAME_TEXT = "Spiel beenden";
-	final String GAME_TITLE_TEXT = "QUIZILLA";
-	
+	// Methoden
 	
 	public int show()
 	{
 		Scanner scanner = null;
 		String userChoice = "";
 		int userChoiceNumber = 0;
-		try
-		{
-			scanner = new Scanner(System.in);
-		}
-		catch(Exception e)
-		{
-			System.out.println("Scanner fehler!");
-		}
+		boolean validUserChoice = false;
+		scanner = new Scanner(System.in);
 		
-		System.out.println("##############" + "[" +  GAME_TITLE_TEXT + "]" + "##############");
-		System.out.println("--------------------------------------");
-		System.out.println("         1) " + PLAY_GAME_TEXT);
-		System.out.println("         2) " + MANUAL_TEXT);
-		System.out.println("         3) " + HIGH_SCORE_TEXT);
-		System.out.println("         4) " + EXIT_GAME_TEXT);
-		System.out.println("--------------------------------------");
-		System.out.print("Auswahl: ");
-		userChoice = scanner.nextLine();
+		
+		System.out.println(UiDesigner.createTitle(DesignConfig.TITLE_QUIZILLA));
+		System.out.println(UiDesigner.createDelimiterLine(DesignConfig.FILL_CHAR_FOR_DELIMITING, DesignConfig.BORDER_CHAR_DELIMITER_LINE));
+		System.out.println(UiDesigner.createMenuLine(DesignConfig.MENU_NUMBER_ONE_TEXT + DesignConfig.MENU_PLAY_GAME));
+		System.out.println(UiDesigner.createMenuLine(DesignConfig.MENU_NUMBER_TWO_TEXT + DesignConfig.MENU_MANUAL));
+		System.out.println(UiDesigner.createMenuLine(DesignConfig.MENU_NUMBER_THREE_TEXT + DesignConfig.MENU_HIGH_SCORE));
+		System.out.println(UiDesigner.createMenuLine(DesignConfig.MENU_NUMBER_FOUR_TEXT + DesignConfig.MENU_EXIT_GAME));
+		System.out.println(UiDesigner.createDelimiterLine(DesignConfig.FILL_CHAR_FOR_DELIMITING, DesignConfig.BORDER_CHAR_DELIMITER_LINE));
+		
+		
+		while(!validUserChoice)
+		{
+			System.out.print(UiDesigner.createUserRequest(DesignConfig.USER_REQUEST_ANSWER_MENU));
+			userChoice = scanner.next();
+			validUserChoice = (userChoice.equals(String.valueOf(DesignConfig.MENU_NUMBER_ONE)) || (userChoice.equals(String.valueOf(DesignConfig.MENU_NUMBER_TWO)) || 
+							  (userChoice.equals(String.valueOf(DesignConfig.MENU_NUMBER_THREE)) || (userChoice.equals(String.valueOf(DesignConfig.MENU_NUMBER_FOUR))))));
+			
+			if(!validUserChoice)
+			{
+				System.out.println(UiDesigner.createInvalidInputMsg(DesignConfig.INVALID_MENU_CHOICE));
+			}
+		}
 		userChoiceNumber = Integer.parseInt(userChoice);
-		System.out.println();
+		
 		
 		return userChoiceNumber;
  	}
